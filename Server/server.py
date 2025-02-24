763abb7fc9e2c223c901452d4c20bce1dfb8bcfc
omrirata <omrata94@gmail.com>
2025-02-24 13:46:24 +0200

    .

--- Server/server.py
+++ Server/server.py
@@ -52,11 +52,11 @@ def randomText():
     print(random_text)
     return jsonify({"message": random_text})
 
-# if __name__ == "__main__":
-#     # app.run(debug=True)
-#     print("********connecte to the server*******")
-#     app.run()
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # Use Azure's port, default to 8000
-    app.run(host='0.0.0.0', port=port)
+    # app.run(debug=True)
+    # print("********connecte to the server*******")
+    app.run()
+
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 8000))  # Use Azure's port, default to 8000
+#     app.run(host='0.0.0.0', port=port)
